0be89b781dcf7e6b20d4461bef8745cb6b7009c0
Joe Quattrone <joequattrone5@gmail.com>
2018-10-15 19:06:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -19,7 +19,7 @@ def speak_to_grandma(phrase)
   
 else if phrase !== phrase.upcase
   return "HUH?! SPEAK UP, SONNY!."
-  
+end  
 else if phrase == phrase.upcase 
   return "NO, NOT SINCE 1938!"
 end
