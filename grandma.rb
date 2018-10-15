d50c7221dad337d40f0cc3938040207e0a657a4f
Joe Quattrone <joequattrone5@gmail.com>
2018-10-15 19:08:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  
+  end
   
 else if phrase != phrase.upcase
   return "HUH?! SPEAK UP, SONNY!."
